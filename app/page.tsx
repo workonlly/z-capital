3856bfc7e285274e4cb6979b8a1f63b44237bfcb
workonlly <workonlly335@gmail.com>
2026-06-06 23:03:28 +0530

    gg

--- app/page.tsx
+++ app/page.tsx
@@ -8,11 +8,11 @@ import Cta from "./homepage/cta";
 export default function Home() {
   return (
     <>
-      <main className="!mt-24 md:mt-0">
-      <Hero />
-      <Features />
-      <Expertise />
-      <Cta />
+      <main className="!pt-24 md:pt-0">
+        <Hero />
+        <Features />
+        <Expertise />
+        <Cta />
       </main>
     </>
   );
